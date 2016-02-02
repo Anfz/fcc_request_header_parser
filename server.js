d09c14bed1a6b997edfd88a9d8931c6f9e537397
Christopher Beaver <chrisbvr@gmail.com>
2016-02-02 04:24:42 +0000

    updates index.html to with clementine.js html We should include the image, will get that later

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ var app = express();
 
 routes(app);
 
+
+app.use('/client', express.static(process.cwd() + '/client'));
+
+app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
+
 app.listen(8080, function () {
     console.log('Listening on port 8080...');
 });
