87e1305f49c43da989e25a12c754cefa4ef90d7f
Christopher Beaver <chrisbvr@gmail.com>
2016-02-02 05:05:41 +0000

    headerparser controller

---
+++ app/controllers/headerParser.js
@@ -0,0 +1,9 @@
+'use strict';
+
+function headerParser (data) {
+
+
+  
+}
+
+module.exports = headerParser;
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,6 +1,12 @@
 'use strict';
 
+
+var HeaderParser = require(process.cwd() + '/app/controllers/headerParser.js');
+
 module.exports = function (app) {
+    
+    var headerParser = new HeaderParser();
+    
     app.route('/').get(function (req, res) {
         res.sendFile(process.cwd()  + '/client/index.html');
     });
