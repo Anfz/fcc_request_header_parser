3e3c3911d1598717c542ae30d7963a0f540c9564
Christopher Beaver <chrisbvr@gmail.com>
2016-02-02 04:29:00 +0000

    simple mongo connection

--- server.js
+++ server.js
@@ -1,17 +1,26 @@
 'use strict'; // best practice, enforces stict rules, saves stupid errors
 
 var express = require('express'), 
-    routes = require('./app/routes/index.js');
+    routes = require('./app/routes/index.js'), 
+    mongo  = require('mongodb').MongoClient;
 
 var app = express();
 
-routes(app);
+mongo.connect('mongodb://localhost:27017/template', function (err, db) {
 
+  if (err) {
+      throw new Error('Database failed to connect!');
+  } else {
+      console.log('MongoDB successfully connected on port 27017.');
+  }
 
-app.use('/client', express.static(process.cwd() + '/client'));
+  app.use('/client', express.static(process.cwd() + '/client'));
+  
+  app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
+  
+  routes(app, db);
 
-app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
-
-app.listen(8080, function () {
-    console.log('Listening on port 8080...');
+  app.listen(8080, function () {
+      console.log('Listening on port 8080...');
+  });
 });
