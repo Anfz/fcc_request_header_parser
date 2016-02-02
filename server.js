9a5726d467e6bb43484a988864b0f70fa3fc48c1
Christopher Beaver <chrisbvr@gmail.com>
2016-02-02 05:00:48 +0000

    inital request header

--- server.js
+++ server.js
@@ -6,21 +6,21 @@ var express = require('express'),
 
 var app = express();
 
-mongo.connect('mongodb://localhost:27017/template', function (err, db) {
+/*mongo.connect('mongodb://localhost:27017/template', function (err, db) {
 
   if (err) {
       throw new Error('Database failed to connect!');
   } else {
       console.log('MongoDB successfully connected on port 27017.');
   }
-
+*/
   app.use('/client', express.static(process.cwd() + '/client'));
   
   app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
   
-  routes(app, db);
+  routes(app);
 
   app.listen(8080, function () {
       console.log('Listening on port 8080...');
   });
-});
+//});
